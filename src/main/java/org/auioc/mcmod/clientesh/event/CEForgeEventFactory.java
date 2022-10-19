3b4c370bccded58dcd615bd6c1ae4fff435fdfeb
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-19 23:37:40 +0800

    feat(event): ClientPlayerPermissionLevelChangedEvent

---
+++ src/main/java/org/auioc/mcmod/clientesh/event/CEForgeEventFactory.java
@@ -0,0 +1,16 @@
+package org.auioc.mcmod.clientesh.event;
+
+import org.auioc.mcmod.clientesh.event.impl.ClientPlayerPermissionLevelChangedEvent;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class CEForgeEventFactory {
+
+    private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
+
+    public static void onPermissionLevelChanged(LocalPlayer player, int oldLevel, int newLevel) {
+        BUS.post(new ClientPlayerPermissionLevelChangedEvent(player, oldLevel, newLevel));
+    }
+
+}
