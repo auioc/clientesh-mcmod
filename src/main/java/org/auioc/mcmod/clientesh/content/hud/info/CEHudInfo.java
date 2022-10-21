2bafcd4af71a2cf922444691fcc71e1b4812832e
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-21 17:36:53 +0800

    refactor(hud.info): CEHudLevelInfo

--- src/main/java/org/auioc/mcmod/clientesh/content/hud/info/CEHudInfo.java
+++ src/main/java/org/auioc/mcmod/clientesh/content/hud/info/CEHudInfo.java
@@ -4,12 +4,6 @@ import org.auioc.mcmod.arnicalib.game.chat.TextUtils;
 import org.auioc.mcmod.clientesh.ClientEsh;
 import org.auioc.mcmod.clientesh.api.hud.HudInfo;
 import org.auioc.mcmod.clientesh.api.mixin.IMixinMinecraft;
-import org.auioc.mcmod.clientesh.content.adapter.SeedGetter;
-import org.auioc.mcmod.clientesh.content.hud.info.CEHudConfig.BiomeRC;
-import org.auioc.mcmod.clientesh.content.hud.info.CEHudConfig.DimensionRC;
-import org.auioc.mcmod.clientesh.content.hud.info.CEHudConfig.LightRC;
-import org.auioc.mcmod.clientesh.content.hud.info.CEHudConfig.TargetedBlockRC;
-import net.minecraft.ChatFormatting;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.ClientBrandRetriever;
 import net.minecraft.client.Minecraft;
@@ -19,10 +13,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LightLayer;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -33,16 +23,12 @@ public class CEHudInfo {
 
     public static final HudInfo MINECRAFT_VERSION = HudInfo.create("MINECRAFT_VERSION", CEHudInfo::minecraftVersion);
     public static final HudInfo FPS = HudInfo.create("FPS", CEHudInfo::fps);
-    public static final HudInfo SEED = HudInfo.create("SEED", CEHudInfo::seed);
-    public static final HudInfo DIMENSION = HudInfo.create("DIMENSION", DimensionRC::build, CEHudInfo::dimension, true);
-    public static final HudInfo BIOME = HudInfo.create("BIOME", BiomeRC::build, CEHudInfo::biome, true);
-    public static final HudInfo LIGHT = HudInfo.create("LIGHT", LightRC::build, CEHudInfo::light, true);
-    public static final HudInfo TARGETED_BLOCK = HudInfo.create("TARGETED_BLOCK", TargetedBlockRC::build, CEHudInfo::targetedBlock, true);
 
     // ============================================================================================================== //
     //#region f1
 
     public static void register() {
+        CEHudLevelInfo.init();
         CEHudPositionInfo.init();
         CEHudTimeInfo.init();
         CEHudPlayerInfo.init();
@@ -119,46 +105,6 @@ public class CEHudInfo {
                 MC.options.enableVsync ? " (vsync)" : ""
             )
         );
-    }
-
-    private static Component[] seed() {
-        return (SeedGetter.hasSeed()) ? lines(label("seed").append(format("%s", SeedGetter.get()))) : lines();
-    }
-
-    private static Component dimension() {
-        var id = level().dimension().location();
-        return label("dimension").append(format(DimensionRC.format.get(), i10n(String.format("dimension.%s.%s", id.getNamespace(), id.getPath())), id.toString()));
-    }
-
-    private static Component[] biome() {
-        var b = level().getBiome(blockpos()).unwrapKey();
-        if (b.isPresent()) {
-            var id = b.get().location();
-            return lines(label("biome").append(format(BiomeRC.format.get(), i10n(String.format("biome.%s.%s", id.getNamespace(), id.getPath())), id.toString())));
-        }
-        return lines();
-    }
-
-    private static Component light() {
-        return label("light").append(format(LightRC.format.get(), level().getBrightness(LightLayer.SKY, blockpos()), level().getBrightness(LightLayer.BLOCK, blockpos())));
-    }
-
-    private static Component[] targetedBlock() {
-        var hit = e().pick(TargetedBlockRC.length.get(), 0.0F, false);
-        if (hit.getType() == HitResult.Type.BLOCK) {
-            var pos = ((BlockHitResult) hit).getBlockPos();
-            double distance = Vec3.atCenterOf(e().eyeBlockPosition()).distanceTo(Vec3.atCenterOf(pos));
-            var state = level().getBlockState(pos);
-            var block = state.getBlock();
-            return lines(
-                null,
-                label("targeted_block").withStyle(ChatFormatting.BOLD),
-                value("targeted_block", "xyz").append(format("%d, %d, %d", pos.getX(), pos.getY(), pos.getZ())),
-                value("targeted_block", "distance").append(format("%.2f", distance)),
-                value("targeted_block", "name").append(block.getName().append(format(" (%s)", state.getBlock().getRegistryName().toString())))
-            );
-        }
-        return lines();
     }
 
 }
---
+++ src/main/java/org/auioc/mcmod/clientesh/content/hud/info/CEHudLevelInfo.java
@@ -0,0 +1,112 @@
+package org.auioc.mcmod.clientesh.content.hud.info;
+
+import org.auioc.mcmod.clientesh.api.hud.HudInfo;
+import org.auioc.mcmod.clientesh.content.adapter.SeedGetter;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeConfigSpec.Builder;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
+
+@OnlyIn(Dist.CLIENT)
+public class CEHudLevelInfo extends CEHudInfo {
+
+    protected static void init() {}
+
+    // ============================================================================================================== //
+
+    public static final HudInfo SEED = HudInfo.create("SEED", CEHudLevelInfo::seed);
+    public static final HudInfo DIMENSION = HudInfo.create("DIMENSION", DimensionRC::build, CEHudLevelInfo::dimension, true);
+    public static final HudInfo BIOME = HudInfo.create("BIOME", BiomeRC::build, CEHudLevelInfo::biome, true);
+    public static final HudInfo LIGHT = HudInfo.create("LIGHT", LightRC::build, CEHudLevelInfo::light, true);
+    public static final HudInfo TARGETED_BLOCK = HudInfo.create("TARGETED_BLOCK", TargetedBlockRC::build, CEHudLevelInfo::targetedBlock, true);
+
+    // ============================================================================================================== //
+    //#region supplier
+
+    private static Component[] seed() {
+        return (SeedGetter.hasSeed()) ? lines(label("seed").append(format("%s", SeedGetter.get()))) : lines();
+    }
+
+    private static Component dimension() {
+        var id = level().dimension().location();
+        return label("dimension").append(format(DimensionRC.format.get(), i10n(String.format("dimension.%s.%s", id.getNamespace(), id.getPath())), id.toString()));
+    }
+
+    private static Component[] biome() {
+        var b = level().getBiome(blockpos()).unwrapKey();
+        if (b.isPresent()) {
+            var id = b.get().location();
+            return lines(label("biome").append(format(BiomeRC.format.get(), i10n(String.format("biome.%s.%s", id.getNamespace(), id.getPath())), id.toString())));
+        }
+        return lines();
+    }
+
+    private static Component light() {
+        return label("light").append(format(LightRC.format.get(), level().getBrightness(LightLayer.SKY, blockpos()), level().getBrightness(LightLayer.BLOCK, blockpos())));
+    }
+
+    private static Component[] targetedBlock() {
+        var hit = e().pick(TargetedBlockRC.length.get(), 0.0F, false);
+        if (hit.getType() == HitResult.Type.BLOCK) {
+            var pos = ((BlockHitResult) hit).getBlockPos();
+            double distance = Vec3.atCenterOf(e().eyeBlockPosition()).distanceTo(Vec3.atCenterOf(pos));
+            var state = level().getBlockState(pos);
+            var block = state.getBlock();
+            return lines(
+                null,
+                label("targeted_block").withStyle(ChatFormatting.BOLD),
+                value("targeted_block", "xyz").append(format("%d, %d, %d", pos.getX(), pos.getY(), pos.getZ())),
+                value("targeted_block", "distance").append(format("%.2f", distance)),
+                value("targeted_block", "name").append(block.getName().append(format(" (%s)", state.getBlock().getRegistryName().toString())))
+            );
+        }
+        return lines();
+    }
+
+    //#endregion supplier
+
+    // ============================================================================================================== //
+    //#region config
+
+    private static class DimensionRC {
+        public static ConfigValue<String> format;
+
+        public static void build(final Builder b) {
+            format = b.comment("1: name", "2: id").define("format", "%1$s (%2$s)");
+        }
+    }
+
+    private static class BiomeRC {
+        public static ConfigValue<String> format;
+
+        public static void build(final Builder b) {
+            format = b.comment("1: name", "2: id").define("format", "%1$s (%2$s)");
+        }
+    }
+
+    private static class LightRC {
+        public static ConfigValue<String> format;
+
+        public static void build(final Builder b) {
+            format = b.comment("1: sky", "2: block").define("format", "sky %1$d, block %2$d");
+        }
+    }
+
+    private static class TargetedBlockRC {
+        public static DoubleValue length;
+
+        public static void build(final Builder b) {
+            length = b.defineInRange("ray_length", 20.0D, 0.0D, Double.MAX_VALUE);
+        }
+    }
+
+    //#endregion config
+
+}
