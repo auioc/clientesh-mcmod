e49f73266356c1fa8ff2ce8f2056d923f6604f38
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-23 20:20:35 +0800

    fix: add onlyIn markers
    [skip ci]

--- src/main/java/org/auioc/mcmod/clientesh/content/tweak/CETweaksConfig.java
+++ src/main/java/org/auioc/mcmod/clientesh/content/tweak/CETweaksConfig.java
@@ -1,8 +1,11 @@
 package org.auioc.mcmod.clientesh.content.tweak;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 
+@OnlyIn(Dist.CLIENT)
 public class CETweaksConfig {
 
     public static BooleanValue enablePauseScreenTweaks;
--- src/main/java/org/auioc/mcmod/clientesh/content/widget/CEWidgetsConfig.java
+++ src/main/java/org/auioc/mcmod/clientesh/content/widget/CEWidgetsConfig.java
@@ -1,8 +1,11 @@
 package org.auioc.mcmod.clientesh.content.widget;
 
 import org.auioc.mcmod.arnicalib.game.config.ConfigUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ForgeConfigSpec;
 
+@OnlyIn(Dist.CLIENT)
 public class CEWidgetsConfig {
 
     public static void build(final ForgeConfigSpec.Builder b) {
