5c17aff8cf8218cebabba8674e09b2d2282732a8
WakelessSloth56 <wsloth56@foxmail.com>
2022-11-24 11:18:30 +0800

    feat(hud.api): abstract elements

---
+++ src/main/java/org/auioc/mcmod/clientesh/api/hud/element/AbsHudElement.java
@@ -0,0 +1,116 @@
+package org.auioc.mcmod.clientesh.api.hud.element;
+
+import javax.annotation.Nullable;
+import org.auioc.mcmod.clientesh.api.hud.value.IOperableValue;
+import com.google.gson.JsonObject;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public abstract class AbsHudElement implements IHudElement {
+
+    private static final Style.Serializer STYLE_SERIALIZER = new Style.Serializer();
+
+    protected static final Minecraft MC = Minecraft.getInstance();
+
+    @Nullable
+    protected Style style;
+
+    public AbsHudElement() {}
+
+    public AbsHudElement(JsonObject json) {
+        this.style = (json.has("style")) ? STYLE_SERIALIZER.deserialize(json.get("style"), null, null) : null;
+    }
+
+    @Override
+    public Component getText() {
+        return (this.style != null) ? getRawText().withStyle(style) : getRawText();
+    }
+
+    protected abstract MutableComponent getRawText();
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    @Nullable
+    public Style getStyle() {
+        return this.style;
+    }
+
+    protected static MutableComponent format(String format, Object... args) {
+        return new TextComponent(String.format(format, args));
+    }
+
+    // ============================================================================================================== //
+
+
+    public static abstract class Boolean extends AbsHudElement implements IOperableValue.Boolean {
+
+        public Boolean(JsonObject json) {
+            super(json);
+        }
+
+        @Override
+        public abstract boolean booleanValue();
+
+        @Override
+        public MutableComponent getRawText() {
+            return new TextComponent(String.valueOf(booleanValue()));
+        }
+
+    }
+
+    public static abstract class Integer extends AbsHudElement implements IOperableValue.Number {
+
+        public Integer(JsonObject json) {
+            super(json);
+        }
+
+        @Override
+        public abstract double doubleValue();
+
+        @Override
+        public MutableComponent getRawText() {
+            return new TextComponent(String.valueOf((int) doubleValue()));
+        }
+
+    }
+
+    public static abstract class Double extends AbsHudElement implements IOperableValue.Number {
+
+        public Double(JsonObject json) {
+            super(json);
+        }
+
+        @Override
+        public abstract double doubleValue();
+
+        @Override
+        public MutableComponent getRawText() {
+            return new TextComponent(String.valueOf(doubleValue()));
+        }
+
+    }
+
+    public static abstract class Literal extends AbsHudElement {
+
+        public Literal(JsonObject json) {
+            super(json);
+        }
+
+        public abstract String getString();
+
+        @Override
+        public MutableComponent getRawText() {
+            return new TextComponent(getString());
+        }
+
+    }
+
+}
