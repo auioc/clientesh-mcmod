eadf4734277de84112dd3cb092b71bf04c9843b4
WakelessSloth56 <wsloth56@foxmail.com>
2022-11-25 17:12:38 +0800

    feat(hud.api): IFunctionElement, IMultilineElement

---
+++ src/main/java/org/auioc/mcmod/clientesh/api/hud/element/IMultilineElement.java
@@ -0,0 +1,17 @@
+package org.auioc.mcmod.clientesh.api.hud.element;
+
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public interface IMultilineElement extends IHudElement {
+
+    Component[] getLines();
+
+    @Override
+    public default Component getText() {
+        throw new UnsupportedOperationException();
+    }
+
+}
