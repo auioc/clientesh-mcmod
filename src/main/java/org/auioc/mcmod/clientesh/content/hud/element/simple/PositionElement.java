98797080387763854c0c098b954a7f9cb53411f0
WakelessSloth56 <wsloth56@foxmail.com>
2022-11-27 21:31:15 +0800

    feat(hud.element): facing

--- src/main/java/org/auioc/mcmod/clientesh/content/hud/element/simple/PositionElement.java
+++ src/main/java/org/auioc/mcmod/clientesh/content/hud/element/simple/PositionElement.java
@@ -1,11 +1,15 @@
 package org.auioc.mcmod.clientesh.content.hud.element.simple;
 
+import org.auioc.mcmod.arnicalib.game.entity.EntityUtils;
 import org.auioc.mcmod.clientesh.api.hud.element.AbsHudElement.AbsBooleanElement;
 import org.auioc.mcmod.clientesh.api.hud.element.AbsHudElement.AbsDoubleElement;
 import org.auioc.mcmod.clientesh.api.hud.element.AbsHudElement.AbsIntegerElement;
+import org.auioc.mcmod.clientesh.api.hud.element.AbsHudElement.AbsStringElement;
 import org.auioc.mcmod.clientesh.api.hud.element.IHudElement;
 import org.auioc.mcmod.clientesh.content.adapter.SeedGetter;
 import com.google.gson.JsonObject;
+import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -113,6 +117,37 @@ public class PositionElement {
         return new AbsIntegerElement(json) {
             @Override
             public int value() { return chunkPosition.getRegionZ(); }
+        };
+    }
+
+    public static IHudElement yaw(JsonObject json) {
+        return new AbsDoubleElement(json) {
+            @Override
+            public double value() { return Mth.wrapDegrees(player.getYRot()); }
+        };
+    }
+
+    public static IHudElement pitch(JsonObject json) {
+        return new AbsDoubleElement(json) {
+            @Override
+            public double value() { return Mth.wrapDegrees(player.getXRot()); }
+        };
+    }
+
+    public static IHudElement facingAxis(JsonObject json) {
+        return new AbsStringElement(json) {
+            @Override
+            public String value() {
+                var d4 = player.getDirection();
+                return ((d4.getAxisDirection() == Direction.AxisDirection.POSITIVE) ? "+" : "-") + (d4.getAxis().getName().toUpperCase());
+            }
+        };
+    }
+
+    public static IHudElement facingDirection(JsonObject json) {
+        return new AbsStringElement(json) {
+            @Override
+            public String value() { return EntityUtils.getFacing8WindDirection(player).getString(); }
         };
     }
 
